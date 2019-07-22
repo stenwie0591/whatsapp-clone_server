2855dc7525b4f5fa2283ba8379205a6e0b73d0f2
stenwie <stenwie91@hotmail.it>
2019-07-22 11:59:20 +0200

    add an ID param to schema graphql

--- tests/queries/getChats.test.ts
+++ tests/queries/getChats.test.ts
@@ -2,14 +2,18 @@ import { createTestClient } from 'apollo-server-testing';
 import { ApolloServer, gql } from 'apollo-server-express';
 import schema from '../../schema';
 
-describe('Query.chats', () => {
-  it('should fetch all chats', async () => {
+describe('Query.chat', () => {
+  it('should fetch specified chat', async () => {
+
     const server = new ApolloServer({ schema });
+
     const { query } = createTestClient(server);
+
     const res = await query({
+      variables: { chatId: '1' },
       query: gql`
-        query GetChats {
-          chats {
+        query GetChat($chatId: ID!) {
+          chat(chatId: $chatId) {
             id
             name
             picture
@@ -27,4 +31,5 @@ describe('Query.chats', () => {
     expect(res.errors).toBeUndefined();
     expect(res.data).toMatchSnapshot();
   });
+  
 });
