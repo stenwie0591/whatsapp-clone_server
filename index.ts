ba8b755dbdc7a048e803b94a3d911f9349350893
stenwie <stenwie91@hotmail.it>
2019-07-22 10:48:00 +0200

    fetch data from backEnd, add Hooks -> end of step3

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
+import cors from 'cors';
 import express from 'express';
+import { chats } from './db';
 
 const app = express();
 
+app.use(cors());
+
 app.get('/_ping', (req, res) => {
   res.send('pong');
+});
+
+app.get('/chats', (req, res) => {
+  res.json(chats);
 });
 
 const port = process.env.PORT || 4000;
