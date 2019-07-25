6352b33bdb0c28acd3dff38732323822619718bf
stenwie <stenwie91@hotmail.it>
2019-07-25 14:16:07 +0200

    add method GraphQL generator

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -1,16 +1,17 @@
 import { GraphQLDateTime } from 'graphql-iso-date';
-import { chats, messages } from '../db';
+import { Message, chats, messages } from '../db';
+import { Resolvers } from '../types/graphql';
 
-const resolvers = {
+const resolvers: Resolvers = {
   Date: GraphQLDateTime,
   Chat: {
-    messages(chat: any) {
+    messages(chat) {
       return messages.filter(m => chat.messages.includes(m.id));
     },
-    lastMessage(chat: any) {
+    lastMessage(chat) {
       const lastMessage = chat.messages[chat.messages.length - 1];
 
-      return messages.find(m => m.id === lastMessage);
+      return messages.find(m => m.id === lastMessage) || null;
     },
   },
   Query: {
@@ -18,19 +19,19 @@ const resolvers = {
       return chats;
     },
 
-    chat(root: any, { chatId }: any) {
-      return chats.find(c => c.id === chatId);
+    chat(root, { chatId }) {
+      return chats.find(c => c.id === chatId) || null;
     },
   },
 
   Mutation: {
-    addMessage(root: any, { chatId, content }: any) {
+    addMessage(root, { chatId, content }) {
       const chatIndex = chats.findIndex(c => c.id === chatId);
       if (chatIndex === -1) return null;
       const chat = chats[chatIndex];
       const lastMessageId = chat.messages[chat.messages.length - 1];
       const messageId = String(Number(lastMessageId) + 1);
-      const message = {
+      const message: Message = {
         id: messageId,
         createdAt: new Date(),
         content,
